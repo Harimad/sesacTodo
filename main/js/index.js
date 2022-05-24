13aad6ad9b7715f559b139962716ed82565ff83e
Harimad <leesh68481@gmail.com>
2022-05-24 15:41:18 +0900

    73. Express 미들웨어, 라이브러리 설정

--- main/js/index.js
+++ main/js/index.js
@@ -1,9 +1,29 @@
-// exports = {}
-exports.a = 1
-exports.b = function () {
-  console.log('hello')
-}
+const express = require('express')
+const cors = require('cors')
+const compression = require('compression')
+const app = express()
+const port = 3000
 
-// module.exports = {
-//   dummy: 2,
-// }
+// express 미들웨어 설정
+
+// cors 설정
+app.use(cors())
+
+// body json 파싱
+app.use(express.json())
+
+// HTTP 요청 압축
+app.use(compression())
+
+// app.get('/users', function (req, res) {
+//   return res.send('hello')
+// })
+
+app.post('/user', function (req, res) {
+  const name = req.body.name
+  return res.send(name)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
